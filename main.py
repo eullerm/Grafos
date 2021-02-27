ca674387bcc446dbb667ab2850914e35a0bbaf1c
Euller Macena <49864491+eullerm@users.noreply.github.com>
2021-02-26 22:43:11 -0300

    Matrizes criadas.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #Trabalho de APA
-from collections import defaultdict
-
-
+from matrix import Matrix
 
 def main():
     listGraph = input().split(";")
 
     graph = {}
+    sizeMatrix = 0
 
     for connection in listGraph:
         
@@ -14,8 +13,19 @@ def main():
             graph[connection[0]].append(connection[2])
         else:
             graph[connection[0]] = [connection[2]]
+        
+        if(graph.get(connection[2])):
+            graph[connection[2]].append(connection[0])
+        else:
+            graph[connection[2]] = [connection[0]]
+        
+        if int(connection[0]) > sizeMatrix:
+            sizeMatrix = int(connection[0])
+        elif int(connection[2]) > sizeMatrix:
+            sizeMatrix = int(connection[2]) 
 
-    
+    matrix = Matrix(graph, sizeMatrix)
+
     print(listGraph)
     print(graph)
 
