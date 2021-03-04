9585c7cc87caf8a7e92791da43c8a6850ea7ec8d
zamp98 <arthurpimentelzampirolli@gmail.com>
2021-03-03 22:43:06 -0300

    1 - desenho dos vétices do grafo(falta calcular a posição corretamente) 2 - criação da pasta de template

--- main.py
+++ main.py
@@ -2,31 +2,70 @@
 from collections import defaultdict
 import graph
 import matrix
+import math
 
 import pygame
-def window():
+
+
+def window(graph):
+    pygame.init()
+    pygame.display.init()
     background_colour = (255,255,255)
-    (width, height) = (1024, 576)
+    (width, height) = (1280, 720)
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Trabalho de APA')
     screen.fill(background_colour)
     pygame.display.flip()
     running = True
     while running:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        drawGraph(graph, screen)
+        pygame.display.flip()
+
+
+def drawGraph(graph, screen):
+    vertexList = graph.getVertex()
+    allSpritesList = pygame.sprite.Group()
+    Rects = pygame.sprite.Group()
+    sprites = []
+    for s in vertexList:
+        screen.blit(s.sprite, (s.x, s.y))
+
+
 
 
 def buildGraph(data):
     vertexList = []
     edgeList = []
     for i in data:
-        v = graph.Vertex(i, 1)
+        positions = []
+        for j in vertexList:
+            positions.append(j.getPos())
+        p = [10, 10]
+        for j in range(len(positions)):
+            x = 70
+            y = 70
+            a = 0
+            b = 0
+            if p == positions[j]:
+                if j % 2 == 0:
+                    a+=1
+                    if (a > math.sqrt(len(data))):
+                        x+=70
+                else:
+                    b+=1
+                    if (b > math.sqrt(len(data))):
+                        y+=70
+
+            p = [p[0] + x, p[1] + y]
+
+
+        v = graph.Vertex(i, 1,  p[0], p[1], False)
         for j in data[i]:
             e = graph.Edge(i, j, 1)
             edgeList.append(e)
-
         vertexList.append(v)
 
     g = graph.Graph(vertexList, edgeList)
@@ -35,7 +74,6 @@ def buildGraph(data):
 def main():
     print("insira o grafo:")
     listGraph = input().split(";")
-
     graph = {}
 
 from matrix import Matrix
@@ -64,12 +102,12 @@ def main():
         elif int(connection[2]) > sizeMatrix:
             sizeMatrix = int(connection[2]) 
 
-        matrix = Matrix(graph, sizeMatrix)
+    matrix = Matrix(graph, sizeMatrix)
     g = buildGraph(graph)
     g.printGraph()
     print(listGraph)
     print(graph)
-    window()
+    window(g)
 
 
 main()
